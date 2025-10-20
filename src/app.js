5431daf63769aeec74a30df96f43c4a3853f299c
borkot9090 <keyafashion659@gmail.com>
2025-10-20 20:58:01 +0600

    dnjjd

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://stockify-f1765.web.app"],
+    origin: ["https://stockify-f1765.web.app"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
